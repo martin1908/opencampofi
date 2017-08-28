64dd364693169aa4a0214a93d19b653ccb04236b
Martin <martin_1908@gmail.com>
2017-08-28 11:07:27 -0500

    subiendo

---
+++ js/header.js
@@ -0,0 +1,12 @@
+	$(document).ready(function(){
+		$(window).scroll(function({
+			if( $(this).scrollTop() > 0){
+				$('header').addClass('sticky');
+
+			} else{
+				$('header').removeClass('sticky');
+			}
+		});
+	});
+		
+	
